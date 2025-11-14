2d6fb0678a5f17835959751495926343152d85d0
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2025-11-14 22:51:08 +0200

    Adding TSP readme and fixing code

--- Graph programs/Traveling Salesman Problem/Traveling Salesman Problem.cpp
+++ Graph programs/Traveling Salesman Problem/Traveling Salesman Problem.cpp
@@ -3,7 +3,9 @@
 #include <vector>
 #include <cmath>
 #include <iomanip>
-
+#include <algorithm>
+#include <random>
+#include <chrono>
 
 
 struct Point
@@ -13,9 +15,6 @@ struct Point
     double y;
 };
 
-#include <vector>
-#include <algorithm>
-#include <random>
 
 std::vector<int> generateRandomPermutation(int n) {
     std::vector<int> perm(n);
@@ -62,14 +61,7 @@ double findPath(const std::vector<std::vector<double>>& dist, const std::vector<
     return path;
 }
 
-std::vector<double> computeFitnessScaled(const std::vector<double>& paths) {
-    const double eps = 1e-9;
-    double mx = *std::max_element(paths.begin(), paths.end());
-    std::vector<double> fit(paths.size());
-    for (size_t i = 0; i < paths.size(); ++i)
-        fit[i] = (mx - paths[i]) + eps; 
-    return fit;
-}
+
 
 static std::mt19937& rng() {
     static std::random_device rd;
@@ -78,15 +70,44 @@ static std::mt19937& rng() {
 }
 
 
-int selectTournament(const std::vector<double>& paths, int k = 3) {
+int selectTournamentPaths(const std::vector<double>& paths, int k = 3) {
+    if (paths.empty()) return 0;
+
     std::uniform_int_distribution<int> uni(0, (int)paths.size() - 1);
+
+
     int best = uni(rng());
+
     for (int i = 1; i < k; ++i) {
         int cand = uni(rng());
-        if (paths[cand] < paths[best]) best = cand;
+        if (paths[cand] < paths[best])  
+            best = cand;
     }
+
     return best;
 }
+
+
+void mutateReverse(std::vector<int>& route, double prob = 0.05) {
+    int n = route.size();
+    if (n < 4) return;
+
+    std::uniform_real_distribution<double> chance(0.0, 1.0);
+    if (chance(rng()) < prob) {
+
+        std::uniform_int_distribution<int> pick(0, n - 1);
+
+        int i = pick(rng());
+        int j = pick(rng());
+
+        if (i > j) std::swap(i, j);
+
+        if (j - i < 2) return;
+
+        std::reverse(route.begin() + i, route.begin() + j + 1);
+    }
+}
+
 
 std::vector<int> orderCrossover(const std::vector<int>& p1, const std::vector<int>& p2) {
     int n = p1.size();
@@ -109,22 +130,13 @@ std::vector<int> orderCrossover(const std::vector<int>& p1, const std::vector<in
     return child;
 }
 
-void mutate(std::vector<int>& route, double prob = 0.05) {
-    std::uniform_real_distribution<double> chance(0.0, 1.0);
-    if (chance(rng()) < prob) {
-        std::uniform_int_distribution<int> pick(0, route.size() - 1);
-        int i = pick(rng());
-        int j = pick(rng());
-        std::swap(route[i], route[j]);
-    }
-}
 
 void printPointsNames(std::vector<int>& arr, std::vector<Point>& points, int n)
 {
     for (int i = 0; i < n; ++i)
     {
         std::cout << points[arr[i]].name;
-        if (i != n-1)
+        if (i != n - 1)
             std::cout << " -> ";
     }
     std::cout << '\n';
@@ -132,8 +144,8 @@ void printPointsNames(std::vector<int>& arr, std::vector<Point>& points, int n)
 
 void geneticAlgorithm(const std::vector<std::vector<double>>& dist, std::vector<Point>& points, int n)
 {
-    int population_max = std::max(80, std::min(4 * n, 300));
-    int generations = 100;
+    int population_max = 700;
+    int generations = 500;
 
     // initialisation population
     std::vector<std::vector<int>> population(population_max);
@@ -142,70 +154,93 @@ void geneticAlgorithm(const std::vector<std::vector<double>>& dist, std::vector<
 
     std::vector<double> paths(population_max);
 
+    // --- adaptive mutation ---
+    double mutationProb = 0.05;                 
+    double globalBestLen = std::numeric_limits<double>::infinity();
+    int generationsSinceImprovement = 0;
+    const double eps = 1e-9;
+
     for (int gen = 0; gen < generations; ++gen)
     {
-        // find paths
+        // 1) find paths
         for (int i = 0; i < population_max; ++i)
             paths[i] = findPath(dist, population[i], n);
 
-        // find best
-        int elite_idx = (int)std::distance(paths.begin(),
-            std::min_element(paths.begin(), paths.end()));
+        // 2) find best
+        int elite_idx = (int)std::distance(paths.begin(), std::min_element(paths.begin(), paths.end()));
         auto elite = population[elite_idx];
         double bestLen = paths[elite_idx];
 
-        // fitness
-        auto fitness = computeFitnessScaled(paths);
+        // update global for mutatae up prob
+        if (bestLen + eps < globalBestLen) {
+            globalBestLen = bestLen;
+            generationsSinceImprovement = 0;   
+        }
+        else {
+            generationsSinceImprovement++;
+        }
+
 
         // new population
         std::vector<std::vector<int>> next_pop;
         next_pop.reserve(population_max);
 
-        // save best
+        // save best 
         next_pop.push_back(elite);
 
-        // Selection
+        // Selection + crossover + mutation
         while ((int)next_pop.size() < population_max)
         {
-            int p1 = selectTournament(paths, 3);
-            int p2 = selectTournament(paths, 3);
+            int p1 = selectTournamentPaths(paths, 4);
+            int p2 = selectTournamentPaths(paths, 4);
             while (p2 == p1)
-                p2 = selectTournament(paths, 3);
+                p2 = selectTournamentPaths(paths, 4);
 
-            // Cross-over
+            // ==== MAKE 4 CHILDREN ====
             auto child1 = orderCrossover(population[p1], population[p2]);
             auto child2 = orderCrossover(population[p2], population[p1]);
+            auto child3 = orderCrossover(population[p1], population[p2]);
+            auto child4 = orderCrossover(population[p2], population[p1]);
 
-            // Mutation
-            mutate(child1, 0.05);
-            mutate(child2, 0.05);
+            // ==== MUTATION (reverse) ====
+            mutateReverse(child1, mutationProb);
+            mutateReverse(child2, mutationProb);
+            mutateReverse(child3, mutationProb);
+            mutateReverse(child4, mutationProb);
 
+            // ==== ADD TO NEXT POPULATION ====
             next_pop.push_back(child1);
-            if ((int)next_pop.size() < population_max)
-                next_pop.push_back(child2);
+            if ((int)next_pop.size() < population_max) next_pop.push_back(child2);
+            if ((int)next_pop.size() < population_max) next_pop.push_back(child3);
+            if ((int)next_pop.size() < population_max) next_pop.push_back(child4);
         }
 
         population.swap(next_pop);
 
         // print best 
         if (gen == 0 || gen == generations - 1 || gen % (generations / 10) == 0) {
-            //std::cout << std::fixed << std::setprecision(4)
-            //    << "Generation " << gen << " | Best path length: " << bestLen << "\n";
-            std::cout << bestLen << '\n';
+            std::cout << std::fixed << std::setprecision(15)
+                << bestLen << '\n';
+        }
+
+        if (gen > 0 && gen % 10 == 0 && generationsSinceImprovement >= 10) {
+            mutationProb += 0.05;                 
+            if (mutationProb > 0.95) mutationProb = 0.95;  
         }
     }
+
 
     for (int i = 0; i < population_max; ++i)
         paths[i] = findPath(dist, population[i], n);
 
-    int best_idx = (int)std::distance(paths.begin(),
-        std::min_element(paths.begin(), paths.end()));
+    int best_idx = (int)std::distance(
+        paths.begin(),
+        std::min_element(paths.begin(), paths.end())
+    );
 
-    std::cout << "\nBest route found:\n";
+    std::cout << std::fixed << std::setprecision(15);
     printPointsNames(population[best_idx], points, n);
-    std::cout << "\nLength: " << std::fixed << std::setprecision(4);
     std::cout << paths[best_idx] << "\n";
-
 }
 
 int main()
@@ -219,20 +254,9 @@ int main()
     std::vector<std::vector<double>> dist(n, std::vector<double>(n, 0.0));
 
 
-    for (int i = 0; i < n; ++i) {
-        std::cin >> points[i].name >> points[i].x >> points[i].y;  
-    }
-
+    for (int i = 0; i < n; ++i)
+        std::cin >> points[i].name >> points[i].x >> points[i].y;
 
     makeAllEdges(points, dist, n);
-
-    //std::cout << std::fixed << std::setprecision(3);
-    //for (int i = 0; i < n; ++i) {
-    //    for (int j = 0; j < n; ++j)
-    //        std::cout << dist[i][j] << " ";
-    //    std::cout << "\n";
-    //}
-
     geneticAlgorithm(dist, points, n);
 }
-    
